9139b1bd32fb369a02cb714f7103beea89a120f9
JasonKChow <jason.k.chow@vanderbilt.edu>
2020-10-24 01:12:01 -0500

    Implement mysql

--- dpsReportUtils.py
+++ dpsReportUtils.py
@@ -4,7 +4,10 @@ import os
 import shutil
 import time
 import sys
-
+import sqlalchemy as sql
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from datetime import datetime
 
 bossIDs = {
     15438: 'Vale Guardian',
@@ -165,6 +168,7 @@ def dataPreProcess(permalink):
     # Get json
     jsonData = requests.post(url='https://dps.report/getJson',
                              params={'permalink': permalink})
+    jsonData.encoding = 'UTF-8'
     data = json.loads(jsonData.text)
 
     # Remove extra info
@@ -257,7 +261,231 @@ def dataPreProcess(permalink):
     return data
 
 
-if __name__ == '__main__':
-    massLogUploader(overwrite=False)
-    massDataBuilder(overwrite=True)
+Base = declarative_base()
 
+
+class Encounter(Base):
+    __tablename__ = 'encounters'
+
+    id = sql.Column(sql.String(100), primary_key=True)
+    bossID = sql.Column(sql.Integer)
+    date = sql.Column(sql.Integer)
+    uploadDate = sql.Column(sql.Integer)
+    success = sql.Column(sql.Boolean)
+    duration = sql.Column(sql.Integer)
+    totalDPS = sql.Column(sql.Integer)
+    isCM = sql.Column(sql.Boolean)
+    permalink = sql.Column(sql.String(100))
+
+    playerEntries = relationship('PlayerEntry', back_populates='encounter')
+
+    def __repr__(self):
+        return f"<Encounter(id={self.id}, " \
+               f"date={self.date}, " \
+               f"permalink={self.permalink})>"
+
+
+class PlayerEntry(Base):
+    __tablename__ = 'playerEntries'
+
+    accountName = sql.Column(sql.String(24), primary_key=True)
+    encID = sql.Column(sql.String(100), sql.ForeignKey('encounters.id'),
+                       primary_key=True)
+
+    specialization = sql.Column(sql.String(24))
+    weapons1Main = sql.Column(sql.String(24), nullable=True)
+    weapons1Off = sql.Column(sql.String(24), nullable=True)
+    weapons2Main = sql.Column(sql.String(24), nullable=True)
+    weapons2Off = sql.Column(sql.String(24), nullable=True)
+    buildStats = sql.Column(sql.String(24))
+
+    subgroup = sql.Column(sql.Integer)
+    totalDPS = sql.Column(sql.Integer)
+    powerDPS = sql.Column(sql.Integer)
+    condiDPS = sql.Column(sql.Integer)
+
+    encounter = relationship('Encounter', back_populates='playerEntries')
+
+    def __repr__(self):
+        return f"<PlayerEntry(accountName={self.accountName}, " \
+               f"encID={self.encID}, " \
+               f"specialization={self.specialization}, " \
+               f"totalDPS={self.totalDPS})>"
+
+
+def uploadLog(fileName, session):
+    userToken = 'kltu2he26nvdrk0451atc1s2p2'
+    params = {'json': 1, 'userToken': userToken}
+
+    # Upload log
+    print('Uploading')
+    file = {'file': open(fileName, 'rb')}
+    upload = requests.post(url='https://dps.report/uploadContent',
+                           files=file,
+                           data=params)
+    upload.encoding = 'UTF-8'
+    meta = json.loads(upload.content)
+    print('Upload complete')
+
+    add_log(meta, session)
+
+
+def add_log(inp, session):
+    if type(inp) == str:
+        # Probably a permalink
+        # Get meta data
+        meta = requests.post(url='https://dps.report/getUploadMetadata',
+                             params={'permalink': inp})
+        meta.encoding = 'UTF-8'
+        meta = json.loads(meta.text)
+    else:
+        # It's probably the metadata
+        meta = inp
+
+    # Get log data
+    r = requests.post(url='https://dps.report/getJson',
+                      params={'permalink': meta.get('permalink')})
+    r.encoding = 'UTF-8'
+    log = json.loads(r.text)
+
+    # Conjured Amalgamate fix
+    if log['fightName'] == 'Conjured Amalgamate':
+        log['players'] = [player for player in log['players']
+                          if not player['profession'] == 'Sword']
+
+    # Twins fix
+    if log['fightName'] == 'Twin Largos':
+        for i in range(len(log['players'])):
+            log['players'][i]['dpsTargets'][0][0]['dps'] += \
+                log['players'][i]['dpsTargets'][1][0]['dps']
+            log['players'][i]['dpsTargets'][0][0]['powerDps'] += \
+                log['players'][i]['dpsTargets'][1][0]['powerDps']
+            log['players'][i]['dpsTargets'][0][0]['condiDps'] += \
+                log['players'][i]['dpsTargets'][1][0]['condiDps']
+
+    # Only save if log is success
+    if not (success := log.get('success')):
+        print('Encounter failed, not saving.')
+        return None
+
+    # Fill in encounter meta data
+    if (uniqueID := meta['encounter'].get('uniqueId')) is None:
+        uniqueID = meta.get('permalink')
+
+    # Check whether or not this is unique
+    if session.query(Encounter).filter_by(id=uniqueID).scalar() is not None:
+        print('This has already been uploaded, not saving.')
+        return None
+
+    # Get easy info
+    bossID = log.get('triggerID')
+    date = int(datetime.strptime(log.get('timeEnd')+'00',
+                                 '%Y-%m-%d %H:%M:%S %z').timestamp())
+    uploadDate = meta.get('uploadTime')
+    isCM = meta['encounter'].get('isCm')
+    permalink = meta.get('permalink')
+
+    # Parse duration
+    duration = 0
+    for piece in log.get('duration').split(' '):
+        if 'ms' in piece:  # Round milliseconds
+            duration += round(float(piece[:-2]) / 1000)
+        elif 'm' in piece:  # Minutes
+            duration += int(piece[:-1]) * 60
+        elif 's' in piece:  # Seconds
+            duration += int(piece[:-1])
+
+    # Get total DPS
+    totalDPS = sum([player['dpsTargets'][0][0].get('dps')
+                    for player in log.get('players')])
+
+    # Add encounter instance
+    enc = Encounter(
+        id=uniqueID,
+        bossID=bossID,
+        date=date,
+        uploadDate=uploadDate,
+        success=success,
+        duration=duration,
+        totalDPS=totalDPS,
+        isCM=isCM,
+        permalink=permalink
+    )
+    session.add(enc)
+
+    # Build playerEntries
+    players = log.get('players')
+
+    # Get relevant for each person
+    print('Parsing json')
+    playerData = []
+    for player in players:
+        # Determine build
+        buildStats = []
+        if player.get('healing') != 0 or player.get('concentration') != 0:
+            if player.get('healing') >= player.get('concentration'):
+                buildStats += ['healing']
+            else:
+                buildStats += ['concentration']
+        else:
+            if player.get('condition') > 0:
+                buildStats += ['condition']
+            else:
+                buildStats += ['power']
+
+        if player.get('toughness') == 10:
+            buildStats += ['toughness']
+
+        buildStats = '-'.join(buildStats)
+
+        playerData += [PlayerEntry(
+            accountName=player.get('account'),
+            encID=uniqueID,
+            specialization=player.get('profession'),
+            weapons1Main=player.get('weapons')[0],
+            weapons1Off=player.get('weapons')[1],
+            weapons2Main=player.get('weapons')[2],
+            weapons2Off=player.get('weapons')[3],
+            buildStats=buildStats,
+            subgroup=player.get('group'),
+            totalDPS=player.get('dpsTargets')[0][0].get('dps'),
+            powerDPS=player.get('dpsTargets')[0][0].get('powerDps'),
+            condiDPS=player.get('dpsTargets')[0][0].get('condiDps')
+        )]
+    session.add_all(playerData)
+
+    # Commit all data from this log
+    session.commit()
+
+
+def massDBImport(id_file, session):
+    # Get a list of IDs
+    with open(id_file, 'r') as f:
+        idList = f.readlines()
+
+    # Strip \n
+    idList = [url[0:-1] for url in idList]
+
+    # Process each permalink
+    for permalink in idList:
+        print(f'Working on {permalink}')
+        add_log(permalink, session)
+
+
+if __name__ == '__main__':
+    with open('dbConfig.json') as f:
+        config = json.load(f)
+
+    dbURI = f"mysql+pymysql://{config.get('user')}:" \
+            f"{config.get('password')}@" \
+            f"{config.get('host')}:" \
+            f"{config.get('port')}/" \
+            f"{config.get('db')}"
+
+    engine = sql.create_engine(dbURI)
+
+    Base.metadata.create_all(engine)
+
+    session = sessionmaker(bind=engine)()
+
+    massDBImport('ids.txt', session)
